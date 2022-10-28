d5daa555a28fae13aed25a8bfec3c4ec77c85049
KipyegonB <koechbernard08@gmail.com>
2022-10-28 23:40:53 +0300

    Completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,45 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+  end
+  fido = Dog.new
+  fido.bark
+
+
+
+  class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def sit
+      puts "The Dog is sitting"
+    end
+  end
+  fido = Dog.new
+  fido.sit
+
+  class Person
+    # Class body
+  
+    # Instance Method Definition
+    def talk
+      puts "Hello World!"
+    end
+  end
+  fido = Person.new
+  fido.talk
+
+  class Person
+    # Class body
+  
+    # Instance Method Definition
+    def walk
+      puts "The Person is walking"
+    end
+  end
+  fido = Person.new
+  fido.walk
